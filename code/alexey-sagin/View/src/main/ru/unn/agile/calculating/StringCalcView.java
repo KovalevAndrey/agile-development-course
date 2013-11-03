3f5d3f4a06de23751b32a37a33cc42152fc663d3
Alexey Sagin <neutralnn@gmail.com>
2013-11-03 19:13:26 +0400

    some corrects in View

--- code/alexey-sagin/View/src/main/ru/unn/agile/calculating/StringCalcView.java
+++ code/alexey-sagin/View/src/main/ru/unn/agile/calculating/StringCalcView.java
@@ -12,7 +12,7 @@ import javax.swing.JTextField;
 
 public class StringCalcView {
 
-	private JFrame frmAsd;
+	private JFrame mainFrame;
 	private JTextField txtExpression;
 	private JTextField txtResult;
 	private JButton btnCalculate;
@@ -25,7 +25,7 @@ public class StringCalcView {
 				try {
 					StringCalcView window = new StringCalcView(
 							new StringCalcViewModel());
-					window.frmAsd.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,51 +36,52 @@ public class StringCalcView {
 	public StringCalcView(StringCalcViewModel viewModel) {
 		this.viewModel = viewModel;
 		initialize();
+		backBind();
 	}
 
 	private void initialize() {
-		frmAsd = new JFrame();
-		frmAsd.setResizable(false);
-		frmAsd.setTitle("String Calculator");
-		frmAsd.setBounds(100, 100, 406, 132);
-		frmAsd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmAsd.getContentPane().setLayout(null);
+		mainFrame = new JFrame();
+		mainFrame.setResizable(false);
+		mainFrame.setTitle("String Calculator");
+		mainFrame.setBounds(100, 100, 406, 132);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(null);
 
 		txtExpression = new JTextField();
 		txtExpression.setBounds(81, 9, 209, 20);
-		frmAsd.getContentPane().add(txtExpression);
+		mainFrame.getContentPane().add(txtExpression);
 		txtExpression.setColumns(10);
 
 		JLabel lblExpression = new JLabel("Expression:");
 		lblExpression.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblExpression.setBounds(10, 8, 61, 20);
-		frmAsd.getContentPane().add(lblExpression);
+		mainFrame.getContentPane().add(lblExpression);
 
 		JLabel lblStatusMark = new JLabel("Status:");
 		lblStatusMark.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblStatusMark.setBounds(10, 70, 39, 20);
-		frmAsd.getContentPane().add(lblStatusMark);
+		mainFrame.getContentPane().add(lblStatusMark);
 
 		txtResult = new JTextField();
 		txtResult.setEditable(false);
 		txtResult.setBounds(221, 40, 69, 20);
-		frmAsd.getContentPane().add(txtResult);
+		mainFrame.getContentPane().add(txtResult);
 		txtResult.setColumns(10);
 
 		btnCalculate = new JButton("Calculate");
 		btnCalculate.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnCalculate.setBounds(300, 7, 89, 53);
-		frmAsd.getContentPane().add(btnCalculate);
+		mainFrame.getContentPane().add(btnCalculate);
 
 		JLabel lblResultMark = new JLabel("Result:");
 		lblResultMark.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblResultMark.setBounds(165, 40, 46, 20);
-		frmAsd.getContentPane().add(lblResultMark);
+		mainFrame.getContentPane().add(lblResultMark);
 
 		lblStatus = new JLabel("");
 		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblStatus.setBounds(59, 70, 330, 20);
-		frmAsd.getContentPane().add(lblStatus);
+		mainFrame.getContentPane().add(lblStatus);
 
 		btnCalculate.addActionListener(new ActionListener() {
 
@@ -88,7 +89,7 @@ public class StringCalcView {
 			public void actionPerformed(ActionEvent arg0) {
 				bind();
 				viewModel.processCalculate();
-				unbind();
+				backBind();
 			}
 
 		});
@@ -100,7 +101,7 @@ public class StringCalcView {
 		viewModel.result = txtResult.getText();
 	}
 
-	private void unbind() {
+	private void backBind() {
 		lblStatus.setText(viewModel.status);
 		txtExpression.setText(viewModel.expression);
 		txtResult.setText(viewModel.result);
