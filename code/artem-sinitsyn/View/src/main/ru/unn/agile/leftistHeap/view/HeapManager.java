1555a24e1fd22c248e78cfe32db383a2d151974f
Artem Sinitsyn <sinitsyn.an.92@gmail.com>
2013-11-04 10:20:03 +0400

    Some view changes

--- code/artem-sinitsyn/View/src/main/ru/unn/agile/leftistHeap/view/HeapManager.java
+++ code/artem-sinitsyn/View/src/main/ru/unn/agile/leftistHeap/view/HeapManager.java
@@ -2,6 +2,11 @@ package ru.unn.agile.leftistHeap.view;
 
 import javax.swing.*;
 
+import ru.unn.agile.leftistHeap.viewModel.ViewModel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class HeapManager {
     private JPanel mainPanel;
     private JTextField addKeyTextField;
@@ -10,6 +15,30 @@ public class HeapManager {
     private JButton getButton;
     private JButton deleteButton;
     private JLabel statusLabel;
+    private JLabel minKeyLabel;
+    private JLabel minValueLabel;
+
+    private ViewModel viewModel;
+
+    public HeapManager() {
+        viewModel = new ViewModel();
+    }
+
+    public void bind() {
+        viewModel.keyAdd = addKeyTextField.getText();
+        viewModel.valueAdd = addValueTextField.getText();
+        viewModel.keyGetDel = minKeyLabel.getText();
+        viewModel.valueGetDel = minValueLabel.getText();
+        viewModel.status = statusLabel.getText();
+    }
+
+    public void unbind() {
+        addKeyTextField.setText(viewModel.keyAdd);
+        addValueTextField.setText(viewModel.valueAdd);
+        minKeyLabel.setText(viewModel.keyGetDel);
+        minValueLabel.setText(viewModel.valueGetDel);
+        statusLabel.setText(viewModel.status);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("HeapManager");
