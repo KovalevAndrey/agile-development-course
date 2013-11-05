337c8903f02f0b81dbc9b96a8f99fbe7f0e8a2f4
Ahmatnurov Dinar <ahmatnurov.d@gmail.com>
2013-11-05 19:25:47 +0400

    almost done

--- code/dinar-ahmatnurov/View/src/main/ru/unn/agile/MathStatistic/view/Calculator.java
+++ code/dinar-ahmatnurov/View/src/main/ru/unn/agile/MathStatistic/view/Calculator.java
@@ -1,11 +1,17 @@
 package ru.unn.agile.MathStatistic.view;
 
+import ru.unn.agile.MathStatistic.viewModel.ViewModel;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Calculator {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
-        frame.setContentPane(new Calculator().mainPanel);
+        frame.setContentPane(new Calculator(new ViewModel()).mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -13,7 +19,51 @@ public class Calculator {
 
     private JPanel mainPanel;
     private JTextField inputDataTextField;
-    private JList list1;
+    private JComboBox<ViewModel.Statistic> listOfStatistic;
     private JButton calcItButton;
-    private JTextField textField1;
+    private JTextField outputDataTextField;
+    private JLabel status;
+    private ViewModel viewModel;
+
+    public Calculator(ViewModel viewModel) {
+        this.viewModel = viewModel;
+        backBind();
+
+        loadListOfOperations();
+
+        calcItButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                bind();
+                Calculator.this.viewModel.calcIt();
+                backBind();
+            }
+        });
+
+        KeyAdapter keyListener = new KeyAdapter() {
+            public void keyReleased(KeyEvent e) {
+                bind();
+                Calculator.this.viewModel.processKeyInTextField(e.getKeyCode());
+                backBind();
+            }
+        };
+
+        inputDataTextField.addKeyListener(keyListener);
+    }
+
+    public void bind() {
+        viewModel.inputData = inputDataTextField.getText();
+        viewModel.operation = (ViewModel.Statistic) listOfStatistic.getSelectedItem();
+    }
+
+    public void backBind() {
+        outputDataTextField.setText(viewModel.outputData);
+        status.setText(viewModel.status);
+    }
+
+    private void loadListOfOperations() {
+        ViewModel.Statistic[] operations = ViewModel.Statistic.values();
+        listOfStatistic.setModel(new JComboBox<ViewModel.Statistic>(operations).getModel());
+        status.setText(viewModel.status);
+    }
 }
--- code/dinar-ahmatnurov/ViewModel/src/main/ru/unn/agile/MathStatistic/viewModel/ViewModel.java
+++ code/dinar-ahmatnurov/ViewModel/src/main/ru/unn/agile/MathStatistic/viewModel/ViewModel.java
@@ -2,13 +2,14 @@ package ru.unn.agile.MathStatistic.viewModel;
 
 import ru.unn.agile.MathStatistic.model.MathStatistic;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class ViewModel {
+    public final int ENTER_CODE = 10;
     public String inputData = "";
     public Statistic operation = Statistic.EXPECTEDVALUE;
     public String outputData = "";
+    public String status = Status.WAITING;
+    public boolean isCalculateButtonEnabled = false;
+    private float[] inputArray;
 
     public enum Statistic {
         EXPECTEDVALUE("Expected value"),
@@ -26,26 +27,26 @@ public class ViewModel {
         }
     }
 
-    public class Status {
-        public static final String WAITING = "Please provide input data";
-        public static final String READY = "Press 'Calc it!' or Enter";
-        public static final String BAD_INPUT = "Bad input data";
-        public static final String SUCCESS = "Success";
-    }
-
-    private boolean isInputDataOK(){
-        return false;
-    }
-
-    public void calcIt() {
-        if(isInputDataOK()) {
-
+    public void processKeyInTextField(int keyCode) {
+        if (keyCode == ENTER_CODE) {
+            calcIt();
+        } else {
+            convertToArrayOfDoubles();
         }
     }
 
-    public float[] convertToArrayOfDoubles(String inputData) {
-        float[] inputArray = null;
+    public class Status {
+        public static final String WAITING = "Waiting for input data";
+        public static final String READY = "Press 'Calc it!' or Enter";
+        public static final String BAD_INPUT = "Bad input data";
+        public static final String SUCCESS = "Success";
+        public static final String ERROR = "Internal error";
+    }
+
+    public void convertToArrayOfDoubles() {
         int indexInputArray = 0;
+        isCalculateButtonEnabled = true;
+        status = Status.READY;
           try {
               inputData = inputData.replaceAll("\\s+"," ");
               inputData = inputData.replaceAll("^ ","");
@@ -59,7 +60,47 @@ public class ViewModel {
 
           } catch (Exception e) {
               inputArray = null;
+              isCalculateButtonEnabled = false;
+              status = Status.BAD_INPUT;
           }
+    }
+
+    public float[] getInputArray(){
         return inputArray;
+    }
+
+    public void calcIt(){
+        convertToArrayOfDoubles();
+        if(status.equals(Status.READY)) {
+            float result = 0;
+            status = Status.SUCCESS;
+            switch (operation) {
+                case EXPECTEDVALUE:
+                    result = MathStatistic.calcExpectedValue(inputArray);
+                    break;
+                case VARIANCE:
+                    result = MathStatistic.calcVariance(inputArray);
+                    break;
+                case THIRDCENTRALMOMENT:
+                    result = MathStatistic.calcThirdCentralMoment(inputArray);
+                    break;
+                case FOURTHCENTRALMOMENT:
+                    result = MathStatistic.calcFourthCentralMoment(inputArray);
+                    break;
+                default:
+                    status = Status.ERROR;
+                    isCalculateButtonEnabled = false;
+            }
+
+            if(!status.equals(Status.ERROR)) {
+                try{
+                    outputData = Float.toString(result);
+                }
+                catch (Exception e) {
+                    status = Status.ERROR;
+                    isCalculateButtonEnabled = false;
+                }
+            }
+        }
     }
 }
