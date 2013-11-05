b39a2b904151b3fcd7bc0b0f554e887988c10b11
KateSergeeva <sergeeva.caterina@gmail.com>
2013-11-05 10:44:36 +0300

    Added ViewModel

--- code/kate-sergeeva/Model/src/main/ru/unn/agile/converter/model/Unit.java
+++ code/kate-sergeeva/Model/src/main/ru/unn/agile/converter/model/Unit.java
@@ -2,21 +2,27 @@ package ru.unn.agile.converter.model;
 
 
 public enum Unit {
-    meter(1),
-    decimeter(0.1),
-    centimeter(0.01),
-    kilometer(1000),
-    millimeter(0.001),
-    inch(0.0254),
-    league(4828.032),
-    mile(1609.344),
-    furhlong(201.16),
-    chain(20.1168),
-    rod(5.029),
-    yard(0.9144);
+    meter("meter", 1),
+    decimeter("decimeter", 0.1),
+    centimeter("centimeter", 0.01),
+    kilometer("kilometer", 1000),
+    millimeter("millimeter", 0.001),
+    inch("inch", 0.0254),
+    league("league", 4828.032),
+    mile("mile", 1609.344),
+    furhlong("furhlong", 201.16),
+    chain("chain", 20.1168),
+    rod("rod", 5.029),
+    yard("yard", 0.9144);
 
-    Unit(double factor){
+    Unit(String name, double factor) {
+        this.name = name;
         this.factor = factor;
     }
+
+    public String toString() {
+        return name;
+    }
     public double factor;
+    public String name;
 }
