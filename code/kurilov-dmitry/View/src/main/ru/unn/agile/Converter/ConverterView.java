df8a25072dc769b5e0b56416b8023cf8a2cbf246
KurilovDmitry <shizgarick@gmail.com>
2013-11-06 11:32:34 +0400

    Added folders

---
+++ code/kurilov-dmitry/View/src/main/ru/unn/agile/Converter/ConverterView.java
@@ -0,0 +1,11 @@
+package ru.unn.agile.Converter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Red_Shuhov
+ * Date: 05.11.13
+ * Time: 18:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConverterView {
+}
