70ebe3b8b5ae7d344fc1c251fb23b1fe7dbf3cc0
Sergey Koklyuev <sergey.kokljuev@gmail.com>
2013-11-09 00:22:17 +0400

    fix warning with tests

--- code/sergey-koklyuev/ViewModel/src/test/ru/unn/agile/Polynomial/viewmodel/PolynomialParserTest.java
+++ code/sergey-koklyuev/ViewModel/src/test/ru/unn/agile/Polynomial/viewmodel/PolynomialParserTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import ru.unn.agile.Polynomial.Term;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class PolynomialParserTest {
@@ -74,7 +75,7 @@ public class PolynomialParserTest {
         Term[] expected_terms = new Term[1];
         term.setValues(3, 4);
         expected_terms[0] = term;
-        assertEquals(expected_terms, parser.parsePolynomial("3x^4"));
+        assertArrayEquals(expected_terms, parser.parsePolynomial("3x^4"));
     }
 
     @Test
@@ -86,6 +87,6 @@ public class PolynomialParserTest {
         expected_terms[0] = term1;
         expected_terms[1] = term2;
         expected_terms[2] = term3;
-        assertEquals(expected_terms, parser.parsePolynomial("3x^2 + -5x + 7"));
+        assertArrayEquals(expected_terms, parser.parsePolynomial("3x^2 + -5x + 7"));
     }
 }
