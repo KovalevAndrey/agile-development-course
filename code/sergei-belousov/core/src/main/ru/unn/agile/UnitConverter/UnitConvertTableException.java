80006a4c5c32cdc630b84104aa466081b4f792c0
BeS <belbes122@yandex.ru>
2013-11-24 23:29:34 +0400

    lab2

--- code/sergei-belousov/core/src/main/ru/unn/agile/UnitConverter/UnitConvertTableException.java
+++
@@ -1,6 +0,0 @@
-package ru.unn.agile.UnitConverter;
-
-public class UnitConvertTableException extends Exception {
-    UnitConvertTableException(){ super();}
-    UnitConvertTableException(String msg){ super(msg);}
-}
