7df5e2fcf171a2d1966303e327c4c2d84bdd86e7
Egor92 <novikov.ea@mail.ru>
2013-12-03 21:20:34 +0400

    Fixed bug with junit

--- code/egor-novikov/Infrastructure/src/test/ru/unn/agile/CreditCalculator/infrastructure/RealLoggerTest.java
+++ code/egor-novikov/Infrastructure/src/test/ru/unn/agile/CreditCalculator/infrastructure/RealLoggerTest.java
@@ -11,8 +11,8 @@ import java.io.FileReader;
 import java.util.List;
 import java.util.regex.*;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 public class RealLoggerTest {
