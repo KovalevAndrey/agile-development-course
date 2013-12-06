40ef106b250d44a70eb46de946d83a75610bf6be
sergey zhdanov <serega_04_03@mail.ru>
2013-12-06 21:56:28 +0400

    fix warnings

--- code/sergey-zhdanov/Infrastructure/src/test/ru/unn/agile/BitArray/Insfrastructure/TextLoggerTest.java
+++ code/sergey-zhdanov/Infrastructure/src/test/ru/unn/agile/BitArray/Insfrastructure/TextLoggerTest.java
@@ -14,9 +14,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 public class TextLoggerTest {
     private static final String LoggerFileName = "./log.log";
