a2d044124505ac5501eb17e451386380a97cbb5e
SmorkalovG <smorkalov.g@gmail.com>
2014-01-10 18:12:46 +0400

    Создана View + забытые в прошлом коммите файлы

---
+++ code/grigory-smorkalov/View/src/main/ru/unn/agile/geometry/view/LinePlainIntersectionForm.java
@@ -0,0 +1,24 @@
+package ru.unn.agile.geometry.view;
+
+import javax.swing.*;
+
+/**
+ * Created by geser on 10.01.14.
+ */
+public class LinePlainIntersectionForm {
+    private JButton calcButton;
+    private JTextField lineP1X;
+    private JTextField lineP1Y;
+    private JTextField lineP2X;
+    private JTextField lineP2Y;
+    private JTextField plainPointX;
+    private JTextField plainPointY;
+    private JTextField plainOrtX;
+    private JTextField plainOrtY;
+    private JTextField resultX;
+    private JTextField resultY;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
