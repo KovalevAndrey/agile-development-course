57baba25b753847242f3dfd7b4dfd4265c591a9b
SmorkalovG <smorkalov.g@gmail.com>
2014-01-19 15:02:47 +0400

    Убран ошибочный импорт, из-за которого не строит travis

--- code/grigory-smorkalov/ViewModel/src/test/ru/unn/agile/geometry/viewModel/ViewModelTests.java
+++ code/grigory-smorkalov/ViewModel/src/test/ru/unn/agile/geometry/viewModel/ViewModelTests.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import ru.unn.agile.geometry.Point;
-import sun.security.jca.ProviderList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
